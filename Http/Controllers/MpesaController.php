2609cbf43f70325cb3787718327385a6437452bc
Dedan Irungu <dedanirungu@gmail.com>
2022-09-01 15:15:11 +0300

    update to commands

---
+++ Http/Controllers/MpesaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Mpesa\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Modules\Base\Http\Controllers\BaseController;
+
+class MpesaController extends BaseController
+{
+
+    public function simulate(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+
+    public function stkpush(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+    public function callback(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+}
