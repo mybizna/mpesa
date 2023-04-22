3e7455fb4eaff15b4924aa3adacb57c737d649e3
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:13 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,35 @@
+<?php
+$this->add_right("mpesa", "gateway", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "gateway", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "gateway", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "gateway", "staff", view:true, add:true, edit:true);
+$this->add_right("mpesa", "gateway", "registered", view:true, add:true);
+$this->add_right("mpesa", "gateway", "guest", view:true, );
+
+$this->add_right("mpesa", "payment", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "payment", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "payment", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "payment", "staff", view:true, add:true, edit:true);
+$this->add_right("mpesa", "payment", "registered", view:true, add:true);
+$this->add_right("mpesa", "payment", "guest", view:true, );
+
+$this->add_right("mpesa", "simulate", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "simulate", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "simulate", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "simulate", "staff", view:true, add:true, edit:true);
+$this->add_right("mpesa", "simulate", "registered", view:true, add:true);
+$this->add_right("mpesa", "simulate", "guest", view:true, );
+
+$this->add_right("mpesa", "stkpush", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "stkpush", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "stkpush", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "stkpush", "staff", view:true, add:true, edit:true);
+$this->add_right("mpesa", "stkpush", "registered", view:true, add:true);
+$this->add_right("mpesa", "stkpush", "guest", view:true, );
+
+$this->add_right("mpesa", "webhook", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "webhook", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "webhook", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mpesa", "webhook", "staff", view:true, add:true, edit:true);
+$this->add_right("mpesa", "webhook", "registered", view:true, add:true);
+$this->add_right("mpesa", "webhook", "guest", view:true, );
