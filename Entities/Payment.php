80a4f44f05341fdab536fa7513b5753e5759ed02
Dedan <dedanirungu@gmail.com>
2024-04-12 11:14:51 +0300

    Add rights() method to Gateway, Payment, Simulate, Stkpush, and Webhook entities

--- Entities/Payment.php
+++ Entities/Payment.php
@@ -89,6 +89,12 @@ class Payment extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 }
