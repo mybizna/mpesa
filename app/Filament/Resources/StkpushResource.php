10c3ccd9e87d601880aa3a29ffaef503cd886e7a
Dedan <dedanirungu@gmail.com>
2025-01-23 03:42:02 +0300

    Make Filament Resource Shorter

--- app/Filament/Resources/StkpushResource.php
+++ app/Filament/Resources/StkpushResource.php
@@ -2,12 +2,7 @@
 
 namespace Modules\Mpesa\Filament\Resources;
 
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
-use Modules\Mpesa\Filament\Resources\StkpushResource\Pages;
 use Modules\Mpesa\Models\Stkpush;
 
 class StkpushResource extends BaseResource
@@ -20,102 +15,4 @@ class StkpushResource extends BaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('amount')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('phone')
-                    ->tel()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('reference')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('command')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('merchant_request_id')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('checkout_request_id')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('gateway_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('completed')
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('successful')
-                    ->numeric()
-                    ->default(0),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('amount')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('phone')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('reference')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('description')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('command')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('merchant_request_id')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('checkout_request_id')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('gateway_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('completed')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('successful')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getPages(): array
-    {
-
-        return [
-            'index' => Pages\Listing::route('/'),
-            'create' => Pages\Creating::route('/create'),
-            'edit' => Pages\Editing::route('/{record}/edit'),
-        ];
-    }
 }
--- app/Filament/Resources/WebhookResource.php
+++ app/Filament/Resources/WebhookResource.php
@@ -2,12 +2,7 @@
 
 namespace Modules\Mpesa\Filament\Resources;
 
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
-use Modules\Mpesa\Filament\Resources\WebhookResource\Pages;
 use Modules\Mpesa\Models\Webhook;
 
 class WebhookResource extends BaseResource
@@ -20,79 +15,4 @@ class WebhookResource extends BaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('slug')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('validation_url')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('confirmation_url')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('paybill_till')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('shortcode')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('published')
-                    ->numeric()
-                    ->default(0),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('slug')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('validation_url')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('confirmation_url')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('paybill_till')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('shortcode')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('published')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getPages(): array
-    {
-
-        return [
-            'index' => Pages\Listing::route('/'),
-            'create' => Pages\Creating::route('/create'),
-            'edit' => Pages\Editing::route('/{record}/edit'),
-        ];
-    }
 }
