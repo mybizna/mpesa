f3438d65069f303d709f11c357d64858bf9c725c
Dedan <dedanirungu@gmail.com>
2025-01-31 20:41:13 +0300

    Remove uncessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Mpesa\Http\Controllers\MpesaController;
 
-Route::match(['get', 'post'], 'kemomo/confirm', 'MpesaController@confirm')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::match(['get', 'post'], 'kemomo/validate', 'MpesaController@validate')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::match(['get', 'post'], 'callback', 'MpesaController@validate')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::match(['get', 'post'], 'kemomo/reversal_queue', 'MpesaController@validate')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::match(['get', 'post'], 'kemomo/reversal_result', 'MpesaController@validate')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::match(['get', 'post'], 'kemomo/simulate', 'MpesaController@simulate');
-Route::post('mpesa/paybill', 'MpesaController@paybill')->name('mpesa_paybill');
-Route::post('mpesa/tillno', 'MpesaController@tillno')->name('mpesa_tillno');
-Route::post('mpesa/stkpush', 'MpesaController@stkpush')->name('mpesa_stkpush');
+
+Route::match(['get', 'post'], 'kemomo/confirm',[MpesaController::class, 'confirm'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::match(['get', 'post'], 'kemomo/validate', [MpesaController::class, 'validate'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::match(['get', 'post'], 'callback', [MpesaController::class, 'validate'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::match(['get', 'post'], 'kemomo/reversal_queue', [MpesaController::class, 'validate'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::match(['get', 'post'], 'kemomo/reversal_result', [MpesaController::class, 'validate'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::match(['get', 'post'], 'kemomo/simulate', [MpesaController::class, 'simulate']);
+Route::post('mpesa/paybill', [MpesaController::class, 'paybill'])->name('mpesa_paybill');
+Route::post('mpesa/tillno', [MpesaController::class, 'tillno'])->name('mpesa_tillno');
+Route::post('mpesa/stkpush', [MpesaController::class, 'stkpush'])->name('mpesa_stkpush');
